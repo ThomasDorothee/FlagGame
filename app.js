525cb39a891d61b3d83e6d2616d0d6924a687714
tomdoro <thomas_051998@outlook.fr>
2018-03-08 17:53:43 +0100

    First Commit 😈

---
+++ app.js
@@ -0,0 +1,25 @@
+var startScreen = document.querySelector('#game-start');
+var startBtn = startScreen.querySelector('button');
+var mainScreen = document.querySelector('#game');
+var gameOver = document.querySelector('#game-over');
+var restartBtn = gameOver.querySelector('button');
+
+var functionTime = function() {
+
+  var timer = 3;
+  var interval = setInterval(function() {
+    var textTimer = mainScreen.querySelector('.time');
+    timer--;
+    console.log(timer);
+    textTimer.innerHTML = timer + ' s';
+    if (timer === 0) {
+      clearInterval(interval);
+      gameOver.style.display = 'block';
+    };
+  }, 1000);
+}
+
+startBtn.addEventListener('click', function() {
+  startScreen.style.display = 'none';
+  functionTime();
+});
